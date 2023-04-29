034c30e8488ae69e81ea7c37d932c826ea1a1d1e
bharavsai15 <bhargavcme123@gmail.com>
2023-04-29 17:20:03 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,95 @@
 // Function to load the Data and display to the user
+async function loadMenuDetails(menuDetails) {
+    let innerHTMLString = '';
+    const cardContainer = document.querySelector('.cards-container');
+    for (let i = 0; i < menuDetails.length; i++){
+        const image = menuDetails[i].imgSrc;
+        const name = menuDetails[i].name;
+        const price = menuDetails[i].price;
+        innerHTMLString += `<div class="card">
+                            <img src="${image}" alt="foot-item">
+                            <div class="item-info">
+                                <p>${name}</p>
+                                <p>Price : $ ${price}</p>
+                            </div>
+                            <button class="btn">Add to cart</button>
+                        </div>`;
+    }
+    cardContainer.innerHTML = innerHTMLString;
+}
+async function getMenu() {
+    const response = await fetch(`https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json`);
+    return response.json();
+}
+async function takeOrder() {
+    let orderDetails = {
+        order_items:[],
+        order_status:false,
+        paid:false
+    };
+    
+    return await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const burgers = ["Cheeseburger", "Veggie Burger", "Chicken Burger"];
+            orderDetails.order_items = burgers.sort();
+            resolve(orderDetails);
+        }, 2500);
+    })
+}
 
+async function orderPrep(orderDetails) {
+    return await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            orderDetails.order_status = true;
+            resolve(orderDetails);
+        }, 1500);
+    })
+}
+async function payOrder(orderDetails) {
+    return await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            orderDetails.paid = true;
+            resolve(orderDetails);
+        }, 1000);
+    })
+}
+function thankyouFnc() {
+    alert("Thank you for eating with us today!");
+}
+
+//Using Promise Chaining
+document.addEventListener('DOMContentLoaded', async () => {
+    const menuDetails = await getMenu();
+    loadMenuDetails(menuDetails);
+    takeOrder().
+        then(function (orderDetails) {
+        console.log("Order details: ",orderDetails);
+        return orderDetails;
+        })
+        .then(function (orderDetails) {
+        return orderPrep(orderDetails);
+        })
+        .then((oderDetails) => {
+        console.log("Order Preparation:",oderDetails);
+        return payOrder(oderDetails);
+        })
+        .then((oderDetails) => {
+        console.log("Payment Status: ",oderDetails);
+            return thankyouFnc();
+        })
+})
+
+
+
+
+// ----------------------------------------------------------------------------
+
+
+//ANOTHER APPROACH
+
+// Function to load the Data and display to the user
+
+/*
 async function loadMenuDetails(menuDetails) {
     let innerHTMLString = '';
     const cardContainer = document.querySelector('.cards-container');
@@ -88,94 +178,6 @@ document.addEventListener('DOMContentLoaded', async ()=> {
         console.log("Error during the restaurant visit:", error);
       }
 })
-  
-// ----------------------------------------------------------------------------------------------
-
-/* Below Approach is using promise Chaining*/
-
-
-// Function to load the Data and display to the user
-
-/*async function loadMenuDetails(menuDetails) {
-    let innerHTMLString = '';
-    const cardContainer = document.querySelector('.cards-container');
-    for (let i = 0; i < menuDetails.length; i++){
-        const image = menuDetails[i].imgSrc;
-        const name = menuDetails[i].name;
-        const price = menuDetails[i].price;
-        innerHTMLString += `<div class="card">
-                            <img src="${image}" alt="foot-item">
-                            <div class="item-info">
-                                <p>${name}</p>
-                                <p>Price : $ ${price}</p>
-                            </div>
-                            <button class="btn">Add to cart</button>
-                        </div>`;
-    }
-    cardContainer.innerHTML = innerHTMLString;
-}
-async function getMenu() {
-    const response = await fetch(`https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json`);
-    return response.json();
-}
-async function takeOrder() {
-    let orderDetails = {
-        order_items:[],
-        order_status:false,
-        paid:false
-    };
-    
-    return await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const burgers = ["Cheeseburger", "Veggie Burger", "Chicken Burger"];
-            orderDetails.order_items = burgers.sort();
-            resolve(orderDetails);
-        }, 2500);
-    })
-}
-
-async function orderPrep(orderDetails) {
-    return await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            orderDetails.order_status = true;
-            resolve(orderDetails);
-        }, 1500);
-    })
-}
-async function payOrder(orderDetails) {
-    return await new Promise((resolve, reject) => {
-        setTimeout(() => {
-            orderDetails.paid = true;
-            resolve(orderDetails);
-        }, 1000);
-    })
-}
-function thankyouFnc() {
-    alert("Thank you for eating with us today!");
-}
-document.addEventListener('DOMContentLoaded', async () => {
-    const menuDetails = await getMenu();
-    loadMenuDetails(menuDetails);
-    takeOrder().
-        then(function (orderDetails) {
-        console.log("Order details: ",orderDetails);
-        return orderDetails;
-        })
-        .then(function (orderDetails) {
-        return orderPrep(orderDetails);
-        })
-        .then((oderDetails) => {
-        console.log("Order Preparation:",oderDetails);
-        return payOrder(oderDetails);
-        })
-        .then((oderDetails) => {
-        console.log("Payment Status: ",oderDetails);
-            return thankyouFnc();
-        })
-})
-
-*/
-
-
+  */
 
 
